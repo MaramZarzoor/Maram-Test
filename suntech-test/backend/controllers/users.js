349f45aaa53ba7da6e3cad220e0bd25a13d11870
Maram-Zarzour <maramzrzoor@gmail.com>
2022-08-19 00:30:10 +0400

    tech assessment Maram
    tech assessment Maram

---
+++ suntech-test/backend/controllers/users.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+
+
+exports.fetchAll = async (req, res, next) => {
+    console.log('fetchAll')
+    try {
+        const [allUsers] = await User.fetchAll();
+        res.status(200).json(allUsers);
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+exports.fetchById = async (req, res, next) => {
+    try {
+        const fetchUser = await User.fetchById(req.params.id);
+        res.status(200).json(fetchUser);
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
